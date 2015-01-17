d2341191081744333d075876d49287267a386fef
Morits Wille <moritsbw@live.dk>
2015-01-17 17:07:18 +0100

    Menu update

--- Unity Project/Assets/MenuGui.cs
+++ Unity Project/Assets/MenuGui.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class MenuGui : MonoBehaviour
 {
-	bool isIt = true;
-	void OnGUI () {
-		if (isIt == true)
-		{
-			if (GUI.Button (new Rect (25, 25, 100, 30), "non-Clicked", "label"))
-			{
-				GUI.Label (new Rect(25, 25, 100, 30), "CLicked");
-				isIt = false;
-			}			
-		}
-	}
+
+    void PlayGame()
+    {
+        Application.LoadLevel("2dPlatformer");
+    }
+
 
 }
---
+++ Unity Project/Assets/StickToCorner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToCorner : MonoBehaviour {
+
+	public Transform mainCam; 
+	public float xPos;
+	public float yPos;
+	Transform pos = new Vector3(mainCam.camera.pixelWidth - xPos, mainCam.camera.pixelHeight - yPos, 0);
+
+	void Update () {
+		transform.position = pos;
+	}
