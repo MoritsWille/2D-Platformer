ff8f49eeeb54f7a68322011085d72acbbaab4203
Peter Wille <pewi@dr.dk>
2015-01-20 20:11:42 +0100

    added Settings and Start

---
+++ Unity Project/Assets/Settings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+
+	public string UserName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Awake() {
+
+		DontDestroyOnLoad(transform.gameObject);
+
+	}
+}
---
+++ Unity Project/Assets/Start1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Application.LoadLevel ("Menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
