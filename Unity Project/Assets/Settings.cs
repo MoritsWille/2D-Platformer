544dc645add69fd64192f382c34e38735471d63e
Morits Wille <moritsbw@live.dk>
2015-01-21 23:47:18 +0100

    added way to save username

--- Unity Project/Assets/Settings.cs
+++ Unity Project/Assets/Settings.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class Settings : MonoBehaviour {
 
 	public string UserName;
+	string path = @"Data\NickName.txt";
+
 	// Use this for initialization
 	void Start () {
+
+		using (StreamReader sr = File.OpenText(path) )
+		{
+			UserName = sr.ReadLine();
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		using (StreamWriter sw = File.CreateText(path))
+		{
+			sw.WriteLine(UserName);
+		}
 	}
 	void Awake() {
 
